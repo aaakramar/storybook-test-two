1e5f9407ded9b81b7c7007cae9f411948bcc76cb
Amar Kumar <amar.kumar@leverageedu.com>
2023-06-20 07:25:07 +0000

    Merged in custom-theme (pull request #7)
    Custom theme
    
    * custom theme added
    
    * file name change from yourtheme to customtheme

---
+++ .storybook/customTheme.js
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Univalley OS',
+//   brandUrl: 'https://example.com',
+//   brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,9 @@
+// // .storybook/manager.js
+
+import { addons } from '@storybook/manager-api';
+import { themes } from '@storybook/theming';
+import customTheme from './customTheme';
+
+addons.setConfig({
+  theme: customTheme,
+});
